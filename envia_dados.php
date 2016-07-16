bf1df17b66983bfea733971b860cabf85da32875
luckas0121 <teixeirasilva1995@hotmail.com>
2016-07-16 05:54:27 -0300

    Desafio

--- envia_dados.php
+++ envia_dados.php
@@ -5,11 +5,6 @@ ini_set('default_charset','UTF-8');
 	$conn = mysqli_connect("localhost","root","");
 	mysqli_select_db($conn,"desafio");
 	
-	//$res=mysqli_query($conn,"SELECT * FROM desafio");
-	//$teste = mysqli_num_rows($res);
-	
-	//echo "Teste de linhas, encontradas $teste";
-	
 	$cod = $_POST['cod_mercadoria'];
 	$tipo = $_POST['tipo_mercadoria'];
 	$nome = $_POST['name_mercadoria'];
@@ -17,11 +12,6 @@ ini_set('default_charset','UTF-8');
 	$preco = $_POST['preco'];
 	$tipoNegocio = $_POST['tipo'];
 	
-	//$insert = mysqli_query($conn,'INSERT INTO desafio VALUES ("'.$cod.'","'.
-	//$tipo.'","'.$nome.'","'.$qtd.'","'.$preco.'","'.$tipoNegocio.')');
-	
-	//echo "Dados inseridos com sucesso !";
-
 	$insert = "INSERT INTO desafio VALUES ('$cod','$tipo','$nome',$qtd,$preco,'$tipoNegocio')";
 	if($res = mysqli_query($conn,$insert)>0){
 		echo "Dados inseridos com sucesso !";
@@ -30,14 +20,6 @@ ini_set('default_charset','UTF-8');
 	}
 	
 	mysqli_close($conn);
-
-
-	/*echo "<br/>Código: ".$cod.
-	"<br/>Tipo: ".$tipo.
-	"<br/>Nome: ".$nome.
-	"<br/>Quantidade: ".$qtd.
-	"<br/>Preço: ".$preco.
-	"<br/>Tipo de Negócio: ".$tipoNegocio;*/
 
 ?>
 
